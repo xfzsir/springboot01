f6417709474a550ae3fc0bc28e3e50c7ea24999e
LQ <675343019@qq.com>
2020-06-04 15:50:31 +0800

    初始化

---
+++ src/main/java/com/cykj/springboot01/Springboot01Application.java
@@ -0,0 +1,16 @@
+package com.cykj.springboot01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author LQ
+ */
+@SpringBootApplication
+public class Springboot01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot01Application.class, args);
+    }
+
+}
